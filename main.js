f1e2a143506a91e92e1ac49f83724e119b9a094c
navaking <ngiovannie@outlook.com>
2021-11-09 19:34:56 -0500

    first coomit

---
+++ main.js
@@ -0,0 +1,12 @@
+//Animate SMooth Scroll
+
+$('#view-shoes').on(click, function(){
+    const images = $('#images').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },
+        900
+    );
+});
